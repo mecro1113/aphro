8a0f991f6000e2af71ad64e58d3c6482168c5e74
Student <student@Nests-iMac-7.local>
2026-05-18 11:39:38 +0800

    fix next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,4 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
@@ -13,4 +14,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
